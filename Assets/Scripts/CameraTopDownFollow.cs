84a6a7bb50872aad47e81fe80852da577b8cff6c
James Ransome <jamesransome@msn.com>
2017-11-30 14:03:40 +0000

    Add camera following player ship script

---
+++ Assets/Scripts/CameraTopDownFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraTopDownFollow : MonoBehaviour {
+
+    public Transform Target;
+    public float CameraDistance = 10f;
+
+    void Update ()
+    {
+        if (Target != null)
+        {
+            FollowTarget();
+        }	
+	}
+
+    void FollowTarget()
+    {
+        Vector3 targetPosition = Target.position;
+        targetPosition.z -= CameraDistance;
+        transform.position = targetPosition;
+    }
+}
