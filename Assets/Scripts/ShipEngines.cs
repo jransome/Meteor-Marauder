8e3ed8e7dff4e9c17ba272627e0054c2b7ee81df
James Ransome <jamesransome@msn.com>
2017-11-30 11:49:48 +0000

    Add player ship movement control

---
+++ Assets/Scripts/ShipEngines.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ShipEngines : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float EnginePower = 10f;
+    public float SteeringPower = 1.4f;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+
+        // Move
+        float thrustInput = Input.GetAxisRaw("Vertical");
+        Vector2 thrustVector = transform.up * thrustInput * EnginePower;
+        rb.AddForce(thrustVector);
+
+        // Rotate
+        float steeringInput = Input.GetAxisRaw("Horizontal");
+        float steeringForce = -steeringInput * SteeringPower;
+        rb.AddTorque(steeringForce);
+    }
+}
