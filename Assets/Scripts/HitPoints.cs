01e54312a559e296637d9a3aa7af5093e92c9dca
James Ransome <jamesransome@msn.com>
2017-12-01 11:57:09 +0000

    Add hitpoints and damage handling based on collision

---
+++ Assets/Scripts/HitPoints.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class HitPoints : MonoBehaviour {
+
+    public int StartingHitPoints;
+    public int CurrentHitPoints { get; private set; }
+
+	void Start ()
+    {
+        CurrentHitPoints = StartingHitPoints;	
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        TakeCollisionDamage(collision);
+    }
+    
+    void TakeCollisionDamage(Collision2D collision)
+    {
+        int kineticEnergy = (int)(0.5f * collision.otherRigidbody.mass * collision.relativeVelocity.sqrMagnitude);
+        TakeDamage(kineticEnergy);
+    }
+
+    void TakeDamage(int amount)
+    {
+        Debug.Log("Damaged: " + -amount + "hp");
+        CurrentHitPoints -= amount;
+    }
+}
