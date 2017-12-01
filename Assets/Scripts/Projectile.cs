6b57172a564ed4f66b86da3523c6eb1d982e78ea
James Ransome <jamesransome@msn.com>
2017-12-01 10:32:39 +0000

    Add dumbfire projectile script

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float Speed = 7f;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * Speed;
+    }
+}
