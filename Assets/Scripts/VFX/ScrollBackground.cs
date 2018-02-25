3606becebd79579b5b6444962606b75ae63ba4cb
James Ransome <jamesransome@msn.com>
2018-02-25 16:14:54 +0000

    Starfield background can be made to fit camera dimensions

--- Assets/Scripts/VFX/ScrollBackground.cs
+++ Assets/Scripts/VFX/ScrollBackground.cs
@@ -3,16 +3,25 @@
 public class ScrollBackground : MonoBehaviour {
 
     public Transform cameraTransform;
-    MeshRenderer quadMeshRenderer;
-    Material quadMaterial;
+
+    private Camera mainCamera;
+    private MeshRenderer quadMeshRenderer;
+    private Material quadMaterial;
 
     void Start()
     {
+        mainCamera = Camera.main;
         quadMeshRenderer = GetComponent<MeshRenderer>();
         quadMaterial = quadMeshRenderer.material;
     }
 
     void Update()
+    {
+        ScrollBackgroundTexture();
+        //FitQuadToCamera(); // commented becasue kind of unnecessary, just made quad big instead
+    }
+
+    void ScrollBackgroundTexture()
     {
         Vector2 newOffset = new Vector2(
             cameraTransform.position.x / transform.localScale.x, //dividing by local scale compensates for the scale of quad and 
@@ -20,5 +29,12 @@ public class ScrollBackground : MonoBehaviour {
         );
 
         quadMaterial.SetTextureOffset("_MainTex", newOffset);
+    }
+
+    void FitQuadToCamera()
+    {
+        float cameraViewHeight = mainCamera.orthographicSize * 2;
+        float cameraViewWidth = cameraViewHeight * mainCamera.aspect;
+        transform.localScale = Vector2.one * Mathf.Max(cameraViewHeight, cameraViewWidth); // avoids texture stretching out on just 1 axis by ensuring quad is always square 
     }
 }
